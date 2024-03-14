2f4b3cef88716888c6db253951ac5b86b39ef5c9
Martin Helmut Fieber <info@martin-fieber.se>
2024-03-14 21:06:12 +0100

    Better settings for universal apple builds

--- cmake/UniversalAppleBuild.cmake
+++ cmake/UniversalAppleBuild.cmake
@@ -1,5 +1,8 @@
-# Generate universal executable for Apple hardware
+# Generate universal executable for Apple hardware for Release builds.
 # This file needs to be included before calling `project`.
-if (APPLE AND RELEASE)
-  set(CMAKE_OSX_ARCHITECTURES "arm64;x86_64" CACHE STRING "")
+if (APPLE AND "${CMAKE_GENERATOR}" STREQUAL "Xcode")
+  # Universal build:
+  # set(CMAKE_OSX_ARCHITECTURES "$(ARCHS_STANDARD)")
+  # Build for current arch:
+  set(CMAKE_OSX_ARCHITECTURES "$(NATIVE_ARCH_ACTUAL)")
 endif ()
